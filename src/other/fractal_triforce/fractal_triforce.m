f4d4385301a8a2c263c4b8d2a2f3919a31ad61b3
Andrew Gerst <gerst20051@gmail.com>
2012-10-04 16:35:17 -0400

    organized triforce fractal

---
+++ src/other/fractal_triforce/fractal_triforce.m
@@ -0,0 +1,20 @@
+clear
+clf
+x = rand;
+y = rand;
+plot(x,y);
+hold on
+for it = 1:200000
+	choic = round(rand*2);
+	if choic == 0
+		x = x/2;
+		y = y/2;
+	elseif choic == 1
+		x = (x+1)/2;
+		y = y/2;
+	else
+		x = (x+0.5)/2;
+		y = (y+1)/2;
+	end
+	plot(x,y);
+end
